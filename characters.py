34dede046953b6f7465dffd8a0610a7b558c7ae4
WJhoward <Will@BRUTUS.home>
2014-10-06 21:31:20 +0100

    Added additional monster super classes and resistances

--- characters.py
+++ characters.py
@@ -5,6 +5,15 @@ import stance
 
 class Man(cards.Card, object):
   Stance = stance.Righteous()
+  
+  #Base modifiers (0 = immune, 1 = normal damage, 2 = double damage)
+  #subclasses overwrite
+  fireModifier = 1
+  iceModifier = 1
+  lightingModifier = 1
+  bladeWeapons = 1
+  bluntWeapons = 1
+  projectileWeapons = 1
 
 class Paladin(Man, object):
   HitPoints = 50
@@ -18,24 +27,47 @@ class Archer(Man, object):
 class Priest(Man, object):
   Name = "Priest"
 
-
-
-
 class Monster(cards.Card, object):
+  
   Stance = stance.Corrupt()
+  #Base modifiers (0 = immune, 1 = normal damage, 2 = double damage)
+  #subclasses overwrite
+  fireModifier = 1
+  iceModifier = 1
+  lightingModifier = 1
+  bladeWeapons = 1
+  bluntWeapons = 1
+  projectileWeapons = 1
 
-class Undead(Monster, object):
+class smallMonster(Monster, object):
+    Size = 50
+    Strength = 50
+
+class humanoidMonster(Monster, object):
+    Size = 100
+    Strenght = 50
+  
+class beastMonster(Monster, object):
+    Size = 150
+    Strenth = 150
+    
+class Dragon(Monster, object):
+    Size = 300
+    Strength = 400
+    fireModifier = 0
+
+class Undead(humanoidMonster, object):
   HitPoints = 10
   Name = "Undead"
 
-class Goblin(Monster, object):
+class Goblin(smallMonster, object):
   HitPoints = 50
   MagicPoints = 10
   Attack = 10
   Defence = 10
   Name = "Goblin"
 
-class Dragon(Monster, object):
+class commonDragon(Dragon, object):
   HitPoints = 1000
   MagicPoints = 1000
   Attack = 10
