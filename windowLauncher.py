af1d347e5b9cde68326e35d2a0bf994311b09af2
KingOfTheBlues <mmason75123@gmail.com>
2014-10-07 13:44:59 +0100

    Now asks for name and starts game on command line with that name vs "Demon"

--- windowLauncher.py
+++ windowLauncher.py
@@ -3,6 +3,11 @@ try:
 except ImportError:
     raise ImportError,"The wxPython module is required to run this program"
 
+import maingame
+import player
+import characters
+import roles
+    
 class simpleapp_wx(wx.Frame):
     def __init__(self,parent,id,title):
         wx.Frame.__init__(self,parent,id,title)
@@ -12,7 +17,7 @@ class simpleapp_wx(wx.Frame):
     def initialize(self):
         sizer = wx.GridBagSizer()
 
-        self.entry = wx.TextCtrl(self,-1,value=u"Enter text here.")
+        self.entry = wx.TextCtrl(self,-1,value=u"Enter name here.")
         sizer.Add(self.entry,(0,0),(1,1),wx.EXPAND)
         self.Bind(wx.EVT_TEXT_ENTER, self.OnPressEnter, self.entry)
 
@@ -34,10 +39,21 @@ class simpleapp_wx(wx.Frame):
         self.Show(True)
 
     def OnButtonClick(self,event):
-        self.label.SetLabel( self.entry.GetValue() + " (You clicked the button)" )
+        self.label.SetLabel("Starting game")
         self.entry.SetFocus()
         self.entry.SetSelection(-1,-1)
+        
+        p1Name = str(self.entry.GetValue())
+        p2Name = "Demon"
 
+        p1 = player.Player(p1Name, roles.GrandWizard)
+        p2 = player.Player(p2Name, roles.DarkLord)
+
+        p1.addCardToDeck(characters.Paladin())
+        p2.addCardToDeck(characters.Goblin())
+
+        battle = p1.startBattle(p2)
+        
     def OnPressEnter(self,event):
         self.label.SetLabel( self.entry.GetValue() + " (You pressed ENTER)" )
         self.entry.SetFocus()
